f2677c0bf05044cc5681507b8049ede4dbb80fc6
yo2882 <133942827+yo2882@users.noreply.github.com>
2023-06-28 14:15:18 +0700

    Add comment about program main logic

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/*ask player input of rock,paper,scissors (case insensitive)*/
+
+/*computer randomly choose between rock,paper,scissors*/
+
+/*compare result of player and computer then display the result*/
+
+/*play 5 round ,track result of each round and report the winner at the end*/
