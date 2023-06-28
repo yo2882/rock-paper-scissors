4b76605827ec490b294f51bc0d400dbd7a3b1ba4
yo2882 <133942827+yo2882@users.noreply.github.com>
2023-06-28 17:38:09 +0700

    Add function for playerChoice and loop for retry

--- js/script.js
+++ js/script.js
@@ -1,55 +1,60 @@
 const choice = ["rock","paper","scissors"];
 let gameScore = 0;
 let score = 0;
+
 /*computer randomly choose between rock,paper,scissors*/
 function computerRandomChoice () {
     return choice[Math.floor(Math.random()*choice.length)];
 }
+
+/*ask player input of rock,paper,scissors (case insensitive)*/
+function playerChoice() {
+    let input = prompt ("Rock, Paper, or Scissors: ").toString().toLowerCase();
+    while (input != "rock" && input != "paper" && input != "scissors"){
+        alert ("Wrong input! Please input Rock, Paper, or Scissors");
+        input = prompt ("Rock, Paper, or Scissors: ").toString().toLowerCase();
+    }
+    return input;
+}
+
 /*compare result of player and computer then display the result*/
 function playRound (a,b) {
-    if(a === "rock" || a ===  "paper" || a === "scissors"){
-        switch (a){
-            case "rock" : 
-                if (b === "rock") {score = 0}
-                else if (b === "paper"){score = -1}
-                else {score = 1};
-                break;
-            case "paper" :
-                if (b === "rock"){score = 1}
-                else if (b === "paper"){score = 0}
-                else {score = -1};
-                break;
-            case "scissors" :
-                if (b === "rock"){score = -1}
-                else if (b === "paper"){score = 1}
-                else {score = 0};
-                break;
-        }
-        switch(score){
-            case 1 :
-                alert ("You win the round!");
-                break;
-            case 0 :
-                alert ("You draw the round!");
-                break;
-            case -1 :
-                alert ("You lose the round!");
-                break;
-        } 
-        return score;
+    switch (a){
+        case "rock" : 
+            if (b === "rock") {score = 0}
+            else if (b === "paper"){score = -1}
+            else {score = 1};
+            break;
+        case "paper" :
+            if (b === "rock"){score = 1}
+            else if (b === "paper"){score = 0}
+            else {score = -1};
+            break;
+        case "scissors" :
+            if (b === "rock"){score = -1}
+            else if (b === "paper"){score = 1}
+            else {score = 0};
+            break;
     }
-    else {
-        alert ("Wrong input! Please input Rock, Paper, or Scissors");
-    }
+    switch(score){
+        case 1 :
+            alert ("You win the round!");
+            break;
+        case 0 :
+            alert ("You draw the round!");
+            break;
+        case -1 :
+            alert ("You lose the round!");
+            break;
+    } 
+    return score;
 }
+
 /*play 5 round ,track result of each round and report the winner at the end*/
 for (i=0; i<5 ; i++) {
     let computerSelection = computerRandomChoice ();
-    /*ask player input of rock,paper,scissors (case insensitive)*/
-    let playerSelection = prompt ("Rock, Paper, or Scissors: ").toLowerCase();
+    let playerSelection = playerChoice();
+    playerSelection = playerSelection.toLowerCase();
     playRound (playerSelection,computerSelection);
-    console.log(playerSelection);
-    console.log(computerSelection);
     gameScore += score
 }
-console.log(gameScore);
