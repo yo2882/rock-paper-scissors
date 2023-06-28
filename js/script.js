8b51376700c8bd5db32e1e13dd2acf6f22d1d318
yo2882 <133942827+yo2882@users.noreply.github.com>
2023-06-28 14:18:51 +0700

    Add function for computer random choice

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 /*ask player input of rock,paper,scissors (case insensitive)*/
 
 /*computer randomly choose between rock,paper,scissors*/
+    function computerRandomChoice () {
+        
+    }
     /*create function for outputing random choice*/
     /*create array of 3 choice of rock,paper,scissors*/
     /*randomly pick 1 item from the array*/
