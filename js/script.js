af6ee380d8b167a05842b468f4880f323bc8c1da
yo2882 <133942827+yo2882@users.noreply.github.com>
2023-06-28 14:26:10 +0700

    Add choice array for the game

--- js/script.js
+++ js/script.js
@@ -2,10 +2,8 @@
 
 /*computer randomly choose between rock,paper,scissors*/
     function computerRandomChoice () {
-        
+        let choice = ["rock","paper","scissors"];
     }
-    /*create function for outputing random choice*/
-    /*create array of 3 choice of rock,paper,scissors*/
     /*randomly pick 1 item from the array*/
     /*return result of the array*/
 /*compare result of player and computer then display the result*/
