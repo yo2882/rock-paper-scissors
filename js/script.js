434b1fe45a903ebc9fa71f8fbc48636b872ba7d7
yo2882 <133942827+yo2882@users.noreply.github.com>
2023-06-28 15:31:07 +0700

    Add function to compare result of selection

--- js/script.js
+++ js/script.js
@@ -7,5 +7,25 @@ function computerRandomChoice () {
     return choice[Math.floor(Math.random()*choice.length)];
 }
 /*compare result of player and computer then display the result*/
-
+function playRound (a,b) {
+    let score = 0
+    switch (a){
+        case "rock" : 
+            if (b === "rock"){score = 0}
+            else if (b === "paper"){score = -1}
+            else {score = 1};
+            break;
+        case "paper" :
+            if (b === "rock"){score = 1}
+            else if (b === "paper"){score = 0}
+            else {score = -1};
+            break;
+        case "scissors" :
+            if (b === "rock"){score = -1}
+            else if (b === "paper"){score = 1}
+            else {score = 0};
+            break;
+    }
+    return score;
+}
 /*play 5 round ,track result of each round and report the winner at the end*/
