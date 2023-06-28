d95524b0fd182d0adb4dbf963fffa7b8a326b369
yo2882 <133942827+yo2882@users.noreply.github.com>
2023-06-28 14:43:57 +0700

    Add variable to store computer selection

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const choice = ["rock","paper","scissors"];
-
+let computerSelection = computerRandomChoice ();
 /*ask player input of rock,paper,scissors (case insensitive)*/
+
 /*computer randomly choose between rock,paper,scissors*/
 function computerRandomChoice () {
     return choice[Math.floor(Math.random()*choice.length)];
@@ -8,4 +9,3 @@ function computerRandomChoice () {
 /*compare result of player and computer then display the result*/
 
 /*play 5 round ,track result of each round and report the winner at the end*/
-console.log (computerRandomChoice ());
