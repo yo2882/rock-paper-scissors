384618a4d87d5ba7110d979d6c294c6b56071c51
yo2882 <133942827+yo2882@users.noreply.github.com>
2023-06-28 14:40:11 +0700

    Edit game choice to be global constants

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
-/*ask player input of rock,paper,scissors (case insensitive)*/
+const choice = ["rock","paper","scissors"];
 
+/*ask player input of rock,paper,scissors (case insensitive)*/
 /*computer randomly choose between rock,paper,scissors*/
-    function computerRandomChoice () {
-        let choice = ["rock","paper","scissors"];
-        return choice[Math.floor(Math.random()*choice.length)];
-    }
+function computerRandomChoice () {
+    return choice[Math.floor(Math.random()*choice.length)];
+}
 /*compare result of player and computer then display the result*/
 
 /*play 5 round ,track result of each round and report the winner at the end*/
