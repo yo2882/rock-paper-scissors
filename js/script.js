779c7d07f0626fdf70bc42042a82609993c4c8ce
yo2882 <133942827+yo2882@users.noreply.github.com>
2023-06-28 14:17:39 +0700

    Add comment about logic of random choice

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
 /*ask player input of rock,paper,scissors (case insensitive)*/
 
 /*computer randomly choose between rock,paper,scissors*/
-
+    /*create function for outputing random choice*/
+    /*create array of 3 choice of rock,paper,scissors*/
+    /*randomly pick 1 item from the array*/
+    /*return result of the array*/
 /*compare result of player and computer then display the result*/
 
 /*play 5 round ,track result of each round and report the winner at the end*/
